e8ceeb66962ccae1e01cb10f0ebf43b7387f8f7a
jeremie <jeremie@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2010-11-23 21:20:42 +0000

    Bug dans RemoteDataCacheProvider : get retourne une instance de Entry au lieu d'une instance de ce qu'on lui demande.
    git-svn-id: https://svn.itinerennes.fr/android/trunk@9 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- ItineRennes/src/fr/itinerennes/business/RemoteDataCacheProvider.java
+++ ItineRennes/src/fr/itinerennes/business/RemoteDataCacheProvider.java
@@ -9,153 +9,168 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class RemoteDataCacheProvider {
 
-	/** The map containing the cached values. */
-	private static final ConcurrentHashMap<CacheKey, CacheEntry> cache = new ConcurrentHashMap<CacheKey, CacheEntry>();
+    /** The map containing the cached values. */
+    private static final ConcurrentHashMap<CacheKey, CacheEntry> cache = new ConcurrentHashMap<CacheKey, CacheEntry>();
 
-	/**
-	 * Puts a new value in the cache.
-	 * 
-	 * @param id
-	 *            the identifier of the value
-	 * @param value
-	 *            the value to cache
-	 */
-	public static void put(final Object id, final Object value) {
+    /**
+     * Puts a new value in the cache.
+     * 
+     * @param id
+     *            the identifier of the value
+     * @param value
+     *            the value to cache
+     */
+    public static void put(final Object id, final Object value) {
 
-		cache.put(new CacheKey(id, value.getClass()), new CacheEntry(value));
-	}
+        cache.put(new CacheKey(id, value.getClass()), new CacheEntry(value));
+    }
 
-	/**
-	 * Gets a previously cached value.
-	 * 
-	 * @param <T>
-	 *            the type of the value
-	 * @param clazz
-	 *            the class of the value
-	 * @param id
-	 *            the key of the value
-	 * @return the requested value
-	 */
-	@SuppressWarnings("unchecked")
-	public static <T> T get(final Class<T> clazz, final Object id) {
+    /**
+     * Gets a previously cached value.
+     * 
+     * @param <T>
+     *            the type of the value
+     * @param clazz
+     *            the class of the value
+     * @param id
+     *            the key of the value
+     * @return the requested value
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T get(final Class<T> clazz, final Object id) {
 
-		return (T) cache.get(new CacheKey(id, clazz));
-	}
+        final CacheEntry entry = cache.get(new CacheKey(id, clazz));
+        if (null != entry) {
+            return (T) entry.getValue();
+        } else {
+            return null;
+        }
+    }
 
-	/**
-	 * Checks the cache contains a key.
-	 * 
-	 * @param clazz
-	 *            the class of the value
-	 * @param id
-	 *            the identifier of the value
-	 * @return true if this key exists in the cache
-	 */
-	public static boolean contains(final Class<?> clazz, final Object id) {
+    /**
+     * Checks the cache contains a key.
+     * 
+     * @param clazz
+     *            the class of the value
+     * @param id
+     *            the identifier of the value
+     * @return true if this key exists in the cache
+     */
+    public static boolean contains(final Class<?> clazz, final Object id) {
 
-		return cache.containsKey(new CacheKey(id, clazz));
-	}
+        return cache.containsKey(new CacheKey(id, clazz));
+    }
 
-	/**
-	 * Checks the cache contains a value.
-	 * 
-	 * @param value
-	 *            the value
-	 * @return true if this value exists in the cache
-	 */
-	public static boolean contains(final Object value) {
+    /**
+     * Checks the cache contains a value.
+     * 
+     * @param value
+     *            the value
+     * @return true if this value exists in the cache
+     */
+    public static boolean contains(final Object value) {
 
-		return cache.contains(new CacheEntry(value));
-	}
+        return cache.contains(new CacheEntry(value));
+    }
 
-	/**
-	 * Represents a key for a cached value.
-	 * 
-	 * @author Jérémie Huchet
-	 */
-	private static class CacheKey {
+    /**
+     * Represents a key for a cached value.
+     * 
+     * @author Jérémie Huchet
+     */
+    private static class CacheKey {
 
-		/** The identifier. */
-		private final Object id;
+        /** The identifier. */
+        private final Object id;
 
-		/** The class name of the value this key identifies. */
-		private final String className;
+        /** The class name of the value this key identifies. */
+        private final String className;
 
-		/**
-		 * Creates a key.
-		 * 
-		 * @param id
-		 *            the identifier
-		 * @param clazz
-		 *            the class name of the value this key identifies
-		 */
-		public CacheKey(final Object id, final Class<?> clazz) {
+        /**
+         * Creates a key.
+         * 
+         * @param id
+         *            the identifier
+         * @param clazz
+         *            the class name of the value this key identifies
+         */
+        public CacheKey(final Object id, final Class<?> clazz) {
 
-			this.id = id;
-			this.className = clazz.getName();
-		}
+            this.id = id;
+            this.className = clazz.getName();
+        }
 
-		/*
-		 * (non-javadoc)
-		 * @see java.lang.Object#hashCode()
-		 */
-		@Override
-		public int hashCode() {
+        /*
+         * (non-javadoc)
+         * @see java.lang.Object#hashCode()
+         */
+        @Override
+        public int hashCode() {
 
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((className == null) ? 0 : className.hashCode());
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			return result;
-		}
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((className == null) ? 0 : className.hashCode());
+            result = prime * result + ((id == null) ? 0 : id.hashCode());
+            return result;
+        }
 
-		/*
-		 * (non-javadoc)
-		 * @see java.lang.Object#equals(java.lang.Object)
-		 */
-		@Override
-		public boolean equals(final Object obj) {
+        /*
+         * (non-javadoc)
+         * @see java.lang.Object#equals(java.lang.Object)
+         */
+        @Override
+        public boolean equals(final Object obj) {
 
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			final CacheKey other = (CacheKey) obj;
-			if (className == null) {
-				if (other.className != null)
-					return false;
-			} else if (!className.equals(other.className))
-				return false;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
-			return true;
-		}
-	}
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            final CacheKey other = (CacheKey) obj;
+            if (className == null) {
+                if (other.className != null)
+                    return false;
+            } else if (!className.equals(other.className))
+                return false;
+            if (id == null) {
+                if (other.id != null)
+                    return false;
+            } else if (!id.equals(other.id))
+                return false;
+            return true;
+        }
+    }
 
-	/**
-	 * Contains cache entry metadata.
-	 * 
-	 * @author Jérémie Huchet
-	 */
-	private static class CacheEntry {
+    /**
+     * Contains cache entry metadata.
+     * 
+     * @author Jérémie Huchet
+     */
+    private static class CacheEntry {
 
-		/** The cached value. */
-		private final Object value;
+        /** The cached value. */
+        private final Object value;
 
-		/**
-		 * Creates a new cache entry.
-		 * 
-		 * @param value
-		 *            the cached value
-		 */
-		public CacheEntry(final Object value) {
+        /**
+         * Creates a new cache entry.
+         * 
+         * @param value
+         *            the cached value
+         */
+        public CacheEntry(final Object value) {
 
-			this.value = value;
-		}
-	}
+            this.value = value;
+        }
+
+        /**
+         * Gets the value.
+         * 
+         * @return the value
+         */
+        public Object getValue() {
+
+            return value;
+        }
+    }
 }
