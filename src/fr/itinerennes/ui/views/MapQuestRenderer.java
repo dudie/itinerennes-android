fbdd6da364be908b807d41b89003ceee0d8c7f10
olivier <olivier@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2010-12-24 11:14:14 +0000

    Déplacement des niveaux de zoom pour MapQuest dans des constantes. Autorisation d'utiliser le zoom 18.
    git-svn-id: https://svn.itinerennes.fr/android/trunk/ItineRennes@84 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- src/fr/itinerennes/ui/views/MapQuestRenderer.java
+++ src/fr/itinerennes/ui/views/MapQuestRenderer.java
@@ -6,18 +6,17 @@ import org.andnav.osm.tileprovider.IOpenStreetMapTileProviderCallback;
 import org.andnav.osm.tileprovider.IOpenStreetMapTileProviderCloudmadeTokenCallback;
 import org.andnav.osm.tileprovider.OpenStreetMapTile;
 import org.andnav.osm.views.util.OpenStreetMapRendererBase;
-import org.slf4j.Logger;
-import org.slf4j.impl.ItinerennesLoggerFactory;
+
+import fr.itinerennes.ItineRennesConstants;
 
 public class MapQuestRenderer extends OpenStreetMapRendererBase {
 
     public MapQuestRenderer() {
 
-        super("MapQuest", 4, 17, 8, ".png", "http://otile1.mqcdn.com/tiles/1.0.0/osm/");
+        super("MapQuest", ItineRennesConstants.CONFIG_MINIMUM_ZOOM_MAP,
+                ItineRennesConstants.CONFIG_MAXIMUM_ZOOM_MAP, 8, ".png",
+                "http://otile1.mqcdn.com/tiles/1.0.0/osm/");
     }
-
-    /** The event logger. */
-    private static final Logger LOGGER = ItinerennesLoggerFactory.getLogger(MapQuestRenderer.class);
 
     /**
      * {@inheritDoc}
