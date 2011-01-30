fa77dcd3933d8b12d8e4b1e365f455576d5a05e1
olivier <olivier@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2011-01-30 00:17:44 +0000

    tests pour OBA
    git-svn-id: https://svn.itinerennes.fr/android/trunk@219 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- ItineRennesTest/src/fr/itinerennes/business/service/OneBusAwayServiceTest.java
+++ ItineRennesTest/src/fr/itinerennes/business/service/OneBusAwayServiceTest.java
@@ -1,5 +1,8 @@
 package fr.itinerennes.business.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -10,12 +13,15 @@ import android.test.AndroidTestCase;
 import fr.itinerennes.business.http.oba.OneBusAwayService;
 import fr.itinerennes.exceptions.GenericException;
 import fr.itinerennes.model.oba.ArrivalAndDeparture;
-import fr.itinerennes.model.oba.Schedule;
+import fr.itinerennes.model.oba.ScheduleStopTime;
+import fr.itinerennes.model.oba.StopSchedule;
+import fr.itinerennes.model.oba.TripSchedule;
 
 /**
  * Test class for {@link OneBusAwayService}.
  * 
  * @author Jérémie Huchet
+ * @author Olivier Boudet
  */
 public class OneBusAwayServiceTest extends AndroidTestCase {
 
@@ -28,31 +34,31 @@ public class OneBusAwayServiceTest extends AndroidTestCase {
     /**
      * Test method for {@link OneBusAwayService#getTripDetails(String)}.
      */
-    public void testGetTripDetails() {
+    public final void testGetTripDetails() {
 
         LOGGER.info("testGetTripDetails.start");
 
-        Schedule schedule = null;
+        TripSchedule schedule = null;
         try {
-            schedule = obaService.getTripDetails("1_10000");
+            schedule = obaService.getTripDetails("10000");
         } catch (final GenericException e) {
             LOGGER.error("GenericException", e);
             fail(e.getMessage());
         }
 
         assertNotNull("no schedule returned by the api", schedule);
-        assertEquals("33 stop times should be returned by the api", schedule.getStopTimes().size(),
-                33);
-        assertEquals("the first stop of this trip should be Grand Quartier", schedule
-                .getStopTimes().get(0).getStop().getName(), "Grand Quartier");
+        assertEquals("33 stop times should be returned by the api", 33, schedule.getStopTimes()
+                .size());
+        assertEquals("the first stop of this trip should be Grand Quartier", "Grand Quartier",
+                schedule.getStopTimes().get(0).getStop().getName());
 
         LOGGER.info("testGetTripDetails.end");
     }
-    
+
     /**
      * Test method for {@link OneBusAwayService#getArrivalsAndDeparturesForStop(String)}.
      */
-    public void testGetArrivalsAndDeparturesForStop() {
+    public final void testGetArrivalsAndDeparturesForStop() {
 
         LOGGER.info("testGetArrivalsAndDeparturesForStop.start");
 
@@ -65,8 +71,49 @@ public class OneBusAwayServiceTest extends AndroidTestCase {
         }
 
         assertNotNull("no arrivals and departures returned by the api", arrivalsAndDepartures);
-        
 
         LOGGER.info("testGetArrivalsAndDeparturesForStop.end");
+    }
+
+    /**
+     * Test method for {@link OneBusAwayService#getScheduleForStop(String)}.
+     */
+    public final void testGetScheduleForStop() {
+
+        LOGGER.info("testGetScheduleForStop.start");
+
+        StopSchedule schedule = null;
+        try {
+            final Calendar calendar = Calendar.getInstance();
+            final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            calendar.setTime(sdf.parse("2011-01-27"));
+            schedule = obaService.getScheduleForStop("1_repbottb", calendar.getTime());
+        } catch (final GenericException e) {
+            LOGGER.error("GenericException", e);
+            fail(e.getMessage());
+        } catch (final ParseException e) {
+            LOGGER.error("The date is not in a valid format.", e);
+            fail(e.getMessage());
+        }
+
+        assertNotNull("no schedule returned by the api", schedule);
+
+        assertEquals("143 stop times should be returned by the api", 143, schedule.getStopTimes()
+                .size());
+        assertEquals("the stop id should be 1_repbottb", "1_repbottb", schedule.getStop().getId());
+        assertEquals("the stop name should be République Pré Botté", "République Pré Botté",
+                schedule.getStop().getName());
+        assertEquals("5 lines should be return for this stop", 5, schedule.getStop().getRoutes()
+                .size());
+
+        int cpt = 0;
+        for (final ScheduleStopTime stopTime : schedule.getStopTimes()) {
+            if (stopTime.getRoute().getShortName().equals("50")) {
+                cpt++;
+            }
+        }
+        assertEquals("65 stop times should be returned by the api for line 50", 65, cpt);
+
+        LOGGER.info("testGetScheduleForStop.end");
     }
 }
