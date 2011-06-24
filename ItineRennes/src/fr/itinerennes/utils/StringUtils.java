7447a3a8c475eecb380bd317877ab2386ec9eebd
olivier <olivier@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2011-06-24 19:38:57 +0000

    revert classe StringUtils de la revision 511
    git-svn-id: https://svn.itinerennes.fr/android/trunk@512 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- ItineRennes/src/fr/itinerennes/utils/StringUtils.java
+++ ItineRennes/src/fr/itinerennes/utils/StringUtils.java
@@ -1,6 +1,5 @@
 package fr.itinerennes.utils;
 
-import java.text.Normalizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,60 +41,5 @@ public final class StringUtils {
         } else {
             return null;
         }
-    }
-    
-    /**
-     * Removes all accents from the given string.
-     * 
-     * @param s
-     *            a string
-     * @return the given string without any special character
-     */
-    public static String unAccent(final String s) {
-        final String unAccentString;
-        if (null != s) {
-        	unAccentString = Normalizer.normalize(s, Normalizer.Form.NFD).replaceAll("[\u0300-\u036F]", "");
-        } else {
-            unAccentString = null;
-        }
-        return unAccentString;
-    }
-    
-	/**
-	 * Highlights the query in the given text using specified prefix and suffix.
-	 * <ul>
-	 * <li><b>text</b> Sample string where another string query must be highlighted</li>
-	 * <li><b>query</b> string</li>
-	 * <li><b>prefix</b> [p]</li>
-	 * <li><b>suffix</b> [s]</li>
-	 * </ul>
-	 * The result will be <i>Sample [p]string[s] where another [p]string[s] query must be highlighted</i>.
-	 * 
-	 * @param text
-	 *            the text where query must be highlighted
-	 * @param query
-	 *            the query to highlight in the text
-	 * @param prefix
-	 *            the prefix to use to highlight the text
-	 * @param suffix
-	 *            the suffix to use to highlight the text
-	 */
-    public static String highlight(String text, String query, String prefix, String suffix) {
-    	StringBuilder result = new StringBuilder();
-    	String unaccentText = unAccent(text).toLowerCase();
-    	String unaccentQuery = unAccent(query).toLowerCase().replaceAll("[^a-zA-Z0-9]", ".");
-    	
-    	int start = 0;
-    	int index;
-    	while ((index = unaccentText.indexOf(unaccentQuery, start)) != -1) {
-    		result.append(text.substring(start, index));
-    		result.append(prefix);
-    		result.append(text.substring(index, index + query.length()));
-    		result.append(suffix);
-    		start = index + query.length();
-    	}
-    	result.append(text.substring(start));
-    	
-    	return result.toString();
     }
 }
