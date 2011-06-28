a1c6f13ee8fd22abada73093d2a45b60bcb8310e
jeremie <jeremie@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2011-06-28 11:52:49 +0000

    StringUtils : méthode pour normaliser les chaines UTF-8
    git-svn-id: https://svn.itinerennes.fr/android/trunk/ItineRennes@518 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- src/fr/itinerennes/utils/StringUtils.java
+++ src/fr/itinerennes/utils/StringUtils.java
@@ -1,7 +1,11 @@
 package fr.itinerennes.utils;
 
+import java.lang.reflect.Method;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Some String function utilities.
@@ -9,6 +13,35 @@ import java.util.regex.Pattern;
  * @author Jérémie Huchet
  */
 public final class StringUtils {
+
+    /** The event logger. */
+    private final static Logger LOGGER = LoggerFactory.getLogger(StringUtils.class);
+
+    private final static Method normalizerNormalizeMethod;
+
+    private final static Object formNFDEnumValue;
+
+    /*
+     * Load a java.text.Normalizer instance to normalize UTF8 string and then remove diachritics
+     * characters.
+     */
+    static {
+        Method _normalizerNormalizeMethod = null;
+        Object _formNFDEnumValue = null;
+        try {
+            final Class<?> normalizerClazz = Class.forName("java.text.Normalizer");
+            final Class<?> formClazz = Class.forName("java.text.Normalizer$Form");
+
+            _normalizerNormalizeMethod = normalizerClazz.getMethod("normalize", CharSequence.class,
+                    formClazz);
+
+            _formNFDEnumValue = formClazz.getEnumConstants()[0];
+        } catch (final Throwable t) {
+            LOGGER.info("No java.text.Normalizer instance found, UTF-8 string normalizing is not supported");
+        }
+        normalizerNormalizeMethod = _normalizerNormalizeMethod;
+        formNFDEnumValue = _formNFDEnumValue;
+    }
 
     /**
      * Private constructor to avoid instantiation.
@@ -42,4 +75,42 @@ public final class StringUtils {
             return null;
         }
     }
+
+    /**
+     * Gets whether or not UTF-8 string normalizing is supported.
+     * 
+     * @return true is UTF-8 string normalizing is supported
+     */
+    public static boolean isNormalizeSupported() {
+
+        return null != normalizerNormalizeMethod && null != formNFDEnumValue;
+    }
+
+    /**
+     * Normalizes the given string using the Canonical decomposition form (
+     * <code>java.text.Normalizer.Form.NFD</code>).
+     * 
+     * @param s
+     *            the string to normalize
+     * @return the normalized string
+     */
+    public static String normalize(final String s) {
+
+        try {
+            return (String) normalizerNormalizeMethod.invoke(null, s, formNFDEnumValue);
+        } catch (final Exception e) {
+            throw new UnsupportedOperationException(
+                    "Unable to normalize the given string in this environment", e);
+        }
+    }
+
+    public static String unaccent(final String s) {
+
+        if (isNormalizeSupported()) {
+            return normalize(s).replaceAll("[\u0300-\u036F]", "");
+        } else {
+            return s;
+        }
+    }
+
 }
