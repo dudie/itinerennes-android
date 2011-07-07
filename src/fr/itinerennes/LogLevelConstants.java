3aa0a656d742537cf832008968fc3e37c767f8e5
olivier <olivier@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2011-07-07 00:30:06 +0000

    Modification des numéros de versions et des niveaux de logs après publication.
    git-svn-id: https://svn.itinerennes.fr/android/trunk/ItineRennes@582 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- src/fr/itinerennes/LogLevelConstants.java
+++ src/fr/itinerennes/LogLevelConstants.java
@@ -15,13 +15,13 @@ public final class LogLevelConstants {
     }
 
     /** Enable/disable {@link Log#VERBOSE} level. */
-    public static final boolean TRACE = false;
+    public static final boolean TRACE = true;
 
     /** Enable/disable {@link Log#DEBUG} level. */
-    public static final boolean DEBUG = false;
+    public static final boolean DEBUG = true;
 
     /** Enable/disable {@link Log#INFO} level. */
-    public static final boolean INFO = false;
+    public static final boolean INFO = true;
 
     /** Enable/disable {@link Log#WARN} level. */
     public static final boolean WARN = true;
