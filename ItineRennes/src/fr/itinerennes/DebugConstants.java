c24c5458e8b3b6a7faa3a43adba25bb87fd24d25
olivier <olivier@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2012-01-02 16:47:06 +0000

    Mise à jour des constantes avant release.
    git-svn-id: https://svn.itinerennes.fr/android/branches/0.4.x@774 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- ItineRennes/src/fr/itinerennes/DebugConstants.java
+++ ItineRennes/src/fr/itinerennes/DebugConstants.java
@@ -15,20 +15,20 @@ public final class DebugConstants {
     }
 
     /** Enable/disable Acra bug reports. */
-    public static final boolean ENABLE_ACRA_BUG_REPORTS = false;
+    public static final boolean ENABLE_ACRA_BUG_REPORTS = true;
 
     /** Enable/disable {@link Log#VERBOSE} level. */
-    public static final boolean LOG_TRACE = true;
+    public static final boolean LOG_TRACE = false;
 
     /** Enable/disable {@link Log#LOG_DEBUG} level. */
-    public static final boolean LOG_DEBUG = true;
+    public static final boolean LOG_DEBUG = false;
 
     /** Enable/disable {@link Log#LOG_INFO} level. */
-    public static final boolean LOG_INFO = true;
+    public static final boolean LOG_INFO = false;
 
     /** Enable/disable {@link Log#LOG_WARN} level. */
-    public static final boolean LOG_WARN = true;
+    public static final boolean LOG_WARN = false;
 
     /** Enable/disable {@link Log#LOG_ERROR} level. */
-    public static final boolean LOG_ERROR = true;
+    public static final boolean LOG_ERROR = false;
 }
