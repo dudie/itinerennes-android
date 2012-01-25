c3645645e563c2e4868b6de0a0e98b9b03553425
olivier <olivier@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2012-01-25 21:15:42 +0000

    fixes #636 : new markers based on gtfs at 2012/01/19
    git-svn-id: https://svn.itinerennes.fr/android/trunk@788 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- itinerennes/src/fr/itinerennes/ItineRennesConstants.java
+++ itinerennes/src/fr/itinerennes/ItineRennesConstants.java
@@ -66,7 +66,7 @@ public final class ItineRennesConstants {
      * Database constants.
      */
     /** The database schema version. */
-    public static final int DATABASE_VERSION = 28;
+    public static final int DATABASE_VERSION = 29;
 
     /** Duration of toast messages. */
     public static final int TOAST_DURATION = 5000;
