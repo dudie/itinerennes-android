5b51eaff8efff35985a1a02479ca492a507c5c7d
jeremie <jeremie@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2012-03-06 23:35:50 +0000

    fixes #629 : constants are loaded from itinerennes.properties asset file, values can be overriden in etc/conf/prod/itinerennes.properties.
    git-svn-id: https://svn.itinerennes.fr/android/trunk@892 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- itinerennes/src/fr/itinerennes/Conf.java
+++ itinerennes/src/fr/itinerennes/Conf.java
@@ -1,16 +1,8 @@
 package fr.itinerennes;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.Field;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Easy access to itinerennes.properties configuration.
@@ -19,132 +11,112 @@ import org.slf4j.LoggerFactory;
  */
 public final class Conf {
 
-    /**
-     * Annomation to mark a field to be injected from the configuration file.
-     * 
-     * @author Jérémie Huchet
-     */
-    @Retention(RetentionPolicy.RUNTIME)
-    @Target(ElementType.FIELD)
-    protected @interface Property {
-
-        /**
-         * The property name.
-         */
-        String name() default "default";
-    }
-
-    /** The event logger. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(Conf.class);
-
     /*
      * Map configuration elements.
      */
     /** Default zoom level. */
-    @Property
-    public static int MAP_DEFAULT_ZOOM;
+    public static final int MAP_DEFAULT_ZOOM;
 
     /** Latitude of city Rennes. */
-    @Property
-    public static int MAP_RENNES_LAT;
+    public static final int MAP_RENNES_LAT;
 
     /** Longitude of city Rennes. */
-    @Property
-    public static int MAP_RENNES_LON;
+    public static final int MAP_RENNES_LON;
 
     /**
      * Minimum zoom level displaying detailed overlay items. On higher zoom, a simple circle will be
      * displayed.
      */
-    @Property
-    public static int MAP_MINIMUM_ZOOM_ITEMS;
+    public static final int MAP_MINIMUM_ZOOM_ITEMS;
 
     /**
      * Zoom level for zooming on a precise marker or location.
      */
-    @Property
-    public static int MAP_ZOOM_ON_LOCATION;
+    public static final int MAP_ZOOM_ON_LOCATION;
 
     /*
      * Itinerennes.
      */
     /** URL to get XML file describing Itinerennes versions. */
-    @Property
-    public static String ITINERENNES_VERSION_URL;
+    public static final String ITINERENNES_VERSION_URL;
 
     /** The APK download page URL. */
-    @Property
-    public static String ITINERENNES_DOWNLOAD_PAGE_URL;
+    public static final String ITINERENNES_DOWNLOAD_PAGE_URL;
 
     /*
      * Nominatim.
      */
     /** Nominatim search bounding box offset. */
-    @Property
-    public static int NOMINATIM_SEARCH_OFFSET = 150000;
+    public static final int NOMINATIM_SEARCH_OFFSET;
 
     /*
      * Keolis constants.
      */
     /** Keolis API URL. */
-    @Property
-    public static String KEOLIS_API_URL;
+    public static final String KEOLIS_API_URL;
 
     /** Key for Keolis API. */
-    @Property
-    public static String KEOLIS_API_KEY;
+    public static final String KEOLIS_API_KEY;
 
     /*
      * OneBusAway constants.
      */
     /** OneBusAway API URL. */
-    @Property
-    public static String ONEBUSAWAY_API_URL;
+    public static final String ONEBUSAWAY_API_URL;
 
     /** OneBusAway API version. */
-    @Property
-    public static String ONEBUSAWAY_API_KEY;
+    public static final String ONEBUSAWAY_API_KEY;
 
     /*
      * Database constants.
      */
     /** The database schema version. */
-    @Property
-    public static int DATABASE_SCHEMA_VERSION;
+    public static final int DATABASE_SCHEMA_VERSION;
 
     /*
      * Misc.
      */
     /** True if ACRA reporting is enabled. */
-    @Property
-    public static boolean ACRA_ENABLED;
+    public static final boolean ACRA_ENABLED;
 
     /** Duration of toast messages. */
-    @Property
-    public static final int TOAST_DURATION = 5000;
-
-    static {
-        new Conf();
-    }
+    public static final int TOAST_DURATION;
 
     /**
-     * Reads the configuration file.
+     * Private constructor to avoid instantiation.
      */
     private Conf() {
+
+    }
+
+    static {
 
         final Properties props = new Properties();
         try {
             props.load(Conf.class.getClassLoader().getResourceAsStream(
                     "assets/itinerennes.properties"));
 
-            for (final Field field : this.getClass().getFields()) {
-                final Property prop = field.getAnnotation(Property.class);
-                if (null != prop) {
-                    loadProperty(field, prop, props);
-                }
-            }
+            MAP_DEFAULT_ZOOM = getInteger(props, "map.default.zoom");
+            MAP_RENNES_LAT = getInteger(props, "map.rennes.lat");
+            MAP_RENNES_LON = getInteger(props, "map.rennes.lon");
+            MAP_MINIMUM_ZOOM_ITEMS = getInteger(props, "map.minimum.zoom.items");
+            MAP_ZOOM_ON_LOCATION = getInteger(props, "map.zoom.on.location");
 
-            ACRA_ENABLED = Boolean.valueOf(props.getProperty("acra.enabled"));
+            ITINERENNES_VERSION_URL = getUrl(props, "itinerennes.version.url");
+            ITINERENNES_DOWNLOAD_PAGE_URL = getUrl(props, "itinerennes.download.page.url");
+
+            NOMINATIM_SEARCH_OFFSET = getInteger(props, "nominatim.search.offset");
+
+            KEOLIS_API_URL = getUrl(props, "keolis.api.url");
+            KEOLIS_API_KEY = getString(props, "keolis.api.key");
+
+            ONEBUSAWAY_API_URL = getUrl(props, "onebusaway.api.url");
+            ONEBUSAWAY_API_KEY = getString(props, "onebusaway.api.key");;
+
+            DATABASE_SCHEMA_VERSION = getInteger(props, "database.schema.version");
+
+            ACRA_ENABLED = getBoolean(props, "acra.enabled");
+            TOAST_DURATION = getInteger(props, "toast.duration");
 
         } catch (final Throwable t) {
             throw new IllegalStateException("Can't load itinerennes.properties configuration file",
@@ -152,36 +124,69 @@ public final class Conf {
         }
     }
 
-    private void loadProperty(final Field field, final Property prop, final Properties properties)
-            throws IllegalArgumentException, IllegalAccessException, MalformedURLException {
+    /**
+     * Get a string property and ensure it is not null.
+     * 
+     * @param properties
+     *            the configuration
+     * @param key
+     *            the property name
+     * @return the property value
+     */
+    private static String getString(final Properties properties, final String key) {
 
-        final String name;
-        if ("default".equals(prop.name())) {
-            name = field.getName().toLowerCase().replaceAll("[^a-z0-9]", ".");
-        } else {
-            name = prop.name();
-        }
-
-        if (properties.containsKey(name)) {
-            field.set(null, convertValue(field, properties.getProperty(name)));
-        } else {
-            throw new IllegalStateException(String.format("Missing property named '%s'", name));
-        }
-    }
-
-    private Object convertValue(final Field field, final String stringValue)
-            throws MalformedURLException {
-
-        final Object value;
-        if ("boolean".equals(field.getType().getName()) || Boolean.class.equals(field.getType())) {
-            value = Boolean.valueOf(stringValue);
-        } else if ("int".equals(field.getType().getName()) || Integer.class.equals(field.getType())) {
-            value = Integer.valueOf(stringValue);
-        } else if (URL.class.equals(field.getType())) {
-            value = new URL(stringValue).toString();
-        } else {
-            value = stringValue;
+        final String value = properties.getProperty(key);
+        if (null == value || value.trim().equals("")) {
+            throw new IllegalArgumentException(
+                    String.format("empty value for property '%s'", value));
         }
         return value;
     }
+
+    /**
+     * Get a property as a boolean value.
+     * 
+     * @param properties
+     *            the configuration
+     * @param key
+     *            the property name
+     * @return a boolean value
+     */
+    private static boolean getBoolean(final Properties properties, final String key) {
+
+        return Boolean.valueOf(getString(properties, key));
+    }
+
+    /**
+     * Get an URL property as a string.
+     * 
+     * @param properties
+     *            the configuration
+     * @param key
+     *            the property name
+     * @return the url as a string value
+     * @throws MalformedURLException
+     *             a wrong URL was specified in configuration file
+     */
+    private static String getUrl(final Properties properties, final String key)
+            throws MalformedURLException {
+
+        // instantiate an URL object to validate syntax, but return a string
+        return new URL(getString(properties, key)).toString();
+    }
+
+    /**
+     * Get a property as a integer value.
+     * 
+     * @param properties
+     *            the configuration
+     * @param key
+     *            the property name
+     * @return an integer value
+     */
+    private static int getInteger(final Properties properties, final String key) {
+
+        return Integer.valueOf(getString(properties, key));
+    }
+
 }
