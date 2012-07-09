c7c31650b11901f75d5b1afb901b3f53a763a474
jeremie <jeremie@3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e>
2012-07-09 21:43:30 +0000

    update drop database script for table "routesstops"
    git-svn-id: https://svn.itinerennes.fr/android/trunk@945 3fbbf0ad-bdca-4ea9-bcf4-e3b57373991e

--- itinerennes/assets/database/drop.sql
+++ itinerennes/assets/database/drop.sql
@@ -1,14 +1,13 @@
 DROP TABLE IF EXISTS "markers";
+DROP INDEX IF EXISTS "markers_idx";
+DROP INDEX IF EXISTS "markers_type_idx";
+DROP INDEX IF EXISTS "markers_search_label_idx";
+DROP INDEX IF EXISTS "markers_label_idx";
 
-DROP TABLE IF EXISTS "bike_stations";
-DROP INDEX IF EXISTS "bike_station_id_idx";
-DROP INDEX IF EXISTS "bike_station_lon_idx";
-DROP INDEX IF EXISTS "bike_station_lat_idx";
-
-DROP TABLE IF EXISTS "subway_stations";
-DROP INDEX IF EXISTS "subway_station_id_idx";
-DROP INDEX IF EXISTS "subway_station_lon_idx";
-DROP INDEX IF EXISTS "subway_station_lat_idx";
+DROP TABLE IF EXISTS "routesstops";
+DROP INDEX IF EXISTS "routesstops_idx";
+DROP INDEX IF EXISTS "routesstops_route_idx";
+DROP INDEX IF EXISTS "routesstops_stop_idx";
 
 DROP TABLE IF EXISTS "bookmarks";
 DROP INDEX IF EXISTS "bookmarks_idx";
@@ -21,6 +20,14 @@ DROP TABLE IF EXISTS "bus_stations";
 DROP INDEX IF EXISTS "bus_stations_id_idx";
 DROP INDEX IF EXISTS "bus_stations_lon_idx";
 DROP INDEX IF EXISTS "bus_stations_lat_idx";
+DROP TABLE IF EXISTS "bike_stations";
+DROP INDEX IF EXISTS "bike_station_id_idx";
+DROP INDEX IF EXISTS "bike_station_lon_idx";
+DROP INDEX IF EXISTS "bike_station_lat_idx";
+DROP TABLE IF EXISTS "subway_stations";
+DROP INDEX IF EXISTS "subway_station_id_idx";
+DROP INDEX IF EXISTS "subway_station_lon_idx";
+DROP INDEX IF EXISTS "subway_station_lat_idx";
 DROP TABLE IF EXISTS "cache_metadata";
 DROP INDEX IF EXISTS "index_unique_type_id";
 DROP TABLE IF EXISTS "line_icons";
