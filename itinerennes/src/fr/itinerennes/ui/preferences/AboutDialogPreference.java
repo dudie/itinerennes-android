020bdcef28549a968a65afdf9fb85e60db4a415e
kops <jeremie@dudie.fr>
2013-12-09 19:03:40 +0100

    [androidannotations] #3 retrieve feed version in background

--- itinerennes/src/fr/itinerennes/ui/preferences/AboutDialogPreference.java
+++ itinerennes/src/fr/itinerennes/ui/preferences/AboutDialogPreference.java
@@ -1,16 +1,23 @@
 package fr.itinerennes.ui.preferences;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import android.app.Activity;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.Background;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.UiThread;
 import com.googlecode.androidannotations.annotations.ViewById;
 
 import fr.itinerennes.R;
+import fr.itinerennes.api.client.model.FeedInfo;
+import fr.itinerennes.ui.activity.ItineRennesActivity;
 import fr.itinerennes.utils.VersionUtils;
 
 /**
@@ -19,7 +26,7 @@ import fr.itinerennes.utils.VersionUtils;
  * @author Jérémie Huchet
  */
 @EActivity(R.layout.act_about)
-public class AboutDialogPreference extends Activity {
+public class AboutDialogPreference extends ItineRennesActivity {
 
 	/** The event logger. */
 	private static final Logger LOGGER = LoggerFactory
@@ -28,9 +35,34 @@ public class AboutDialogPreference extends Activity {
 	@ViewById(R.id.about_version_name)
 	protected TextView versionText;
 
+	@ViewById(R.id.about_keolis_version)
+	protected TextView keolisVersion;
+
+	@ViewById(R.id.about_keolis_version_pbar)
+	protected ProgressBar keolisVersionProgressBar;
+
 	@AfterViews
 	protected void setVersionName() {
 		versionText.setText(this.getString(R.string.version_dots,
 				VersionUtils.getCurrent(this)));
+		loadAndUpdateFeedInfo();
+	}
+
+	@Background
+	protected void loadAndUpdateFeedInfo() {
+		try {
+			final FeedInfo fi = getApplicationContext()
+					.getItineRennesApiClient().getFeedInfo();
+			updateKeolisInfos(fi);
+		} catch (final IOException e) {
+			LOGGER.error("can't retrieve feed info", e);
+		}
+	}
+
+	@UiThread
+	protected void updateKeolisInfos(final FeedInfo fi) {
+		keolisVersion.setText(String.format("(%s)", fi.getVersion()));
+		keolisVersion.setVisibility(View.VISIBLE);
+		keolisVersionProgressBar.setVisibility(View.GONE);
 	}
 }
