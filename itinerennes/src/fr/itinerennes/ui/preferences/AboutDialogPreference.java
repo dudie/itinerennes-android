11ef1f9d1c9367bdfcc3f67c6e7fcb3c61925a1c
kops <jeremie@dudie.fr>
2013-12-09 01:41:12 +0100

    [androidannotations] sample with AboutActivity

--- itinerennes/src/fr/itinerennes/ui/preferences/AboutDialogPreference.java
+++ itinerennes/src/fr/itinerennes/ui/preferences/AboutDialogPreference.java
@@ -3,11 +3,12 @@ package fr.itinerennes.ui.preferences;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import android.content.Context;
-import android.preference.DialogPreference;
-import android.util.AttributeSet;
-import android.view.View;
+import android.app.Activity;
 import android.widget.TextView;
+
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
 
 import fr.itinerennes.R;
 import fr.itinerennes.utils.VersionUtils;
@@ -17,52 +18,19 @@ import fr.itinerennes.utils.VersionUtils;
  * 
  * @author Jérémie Huchet
  */
-public final class AboutDialogPreference extends DialogPreference {
+@EActivity(R.layout.act_about)
+public class AboutDialogPreference extends Activity {
 
-    /** The event logger. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(AboutDialogPreference.class);
+	/** The event logger. */
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(AboutDialogPreference.class);
 
-    /**
-     * Constructor.
-     * 
-     * @param context
-     *            the context
-     * @param attrs
-     *            the xml attributes
-     */
-    public AboutDialogPreference(final Context context, final AttributeSet attrs) {
+	@ViewById(R.id.about_version_name)
+	protected TextView versionText;
 
-        super(context, attrs);
-    }
-
-    /**
-     * Constructor.
-     * 
-     * @param context
-     *            the context
-     * @param attrs
-     *            the xml attributes
-     * @param defStyle
-     *            the default style
-     */
-    public AboutDialogPreference(final Context context, final AttributeSet attrs, final int defStyle) {
-
-        super(context, attrs, defStyle);
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see android.preference.DialogPreference#onCreateDialogView()
-     */
-    @Override
-    protected View onCreateDialogView() {
-
-        final View aboutView = super.onCreateDialogView();
-        final TextView versionText = (TextView) aboutView.findViewById(R.id.about_version_name);
-        versionText.setText(getContext().getString(R.string.version_dots,
-                VersionUtils.getCurrent(this.getContext())));
-
-        return aboutView;
-    }
+	@AfterViews
+	protected void setVersionName() {
+		versionText.setText(this.getString(R.string.version_dots,
+				VersionUtils.getCurrent(this)));
+	}
 }
