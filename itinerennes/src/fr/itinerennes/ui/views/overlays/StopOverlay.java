e9c3ab7475c1062aaa31e13f05b795a339413a48
kops <jeremie@dudie.fr>
2013-12-18 22:53:49 +0100

    Mark BAD potential memory leak

--- itinerennes/src/fr/itinerennes/ui/views/overlays/StopOverlay.java
+++ itinerennes/src/fr/itinerennes/ui/views/overlays/StopOverlay.java
@@ -77,6 +77,7 @@ public class StopOverlay extends LazyOverlay implements ILayerSelector {
         visibleMarkerTypes.add(TypeConstants.TYPE_BUS);
         visibleMarkerTypes.add(TypeConstants.TYPE_SUBWAY);
 
+        // FIXME this may produce a memory leak as the listener is never unregistered
         context.getApplicationContext().getBookmarksService()
                 .addListener(new IBookmarkModificationListener() {
 
