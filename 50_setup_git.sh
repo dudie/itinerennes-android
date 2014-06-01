5d094e877c9577ed781f9382d942cd8d31fe8057
kops <jeremie@dudie.fr>
2014-06-01 23:53:41 +0200

    Use dudie-ci git identity

--- 50_setup_git.sh
+++ 50_setup_git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # setup git
-git config --global user.email "dudie.fr+travis+itinerennes+android@gmail.com"
-git config --global user.name "Travis CI"
+git config --global user.email "dudie.fr+github@gmail.com"
+git config --global user.name "dudie-ci"
 
 # register Github identity
 curl "https://raw.github.com/dudie/maven-repository/master/github_key.pub" >> ~/.ssh/known_hosts
