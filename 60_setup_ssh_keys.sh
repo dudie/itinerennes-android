7c4c39a6907ada7764ac7681808b02e82f4f37cb
kops <jeremie@dudie.fr>
2014-06-01 20:31:30 +0200

    debug ssh keys initialization

--- 60_setup_ssh_keys.sh
+++ 60_setup_ssh_keys.sh
@@ -5,7 +5,11 @@ git clone https://gist.github.com/7630453.git travis-tools
 
 # setup private keys
 eval $(ssh-agent)
+
+echo Setting up SSH ids: $SSH_IDS
+
 for id in $SSH_IDS ; do
   cat ${id}.enc | ./travis-tools/decrypt.sh $KEY > $id
   chmod 400 $id
+  ls -l ${id}.enc $id
 done
